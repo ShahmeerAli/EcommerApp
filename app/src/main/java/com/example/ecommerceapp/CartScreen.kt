b10f4a95ff0643d07ad572f37f90a69138aa47f7
ShahmeerAli <shahmeer.devel@gmail.com>
2025-05-19 14:46:15 +0500

    cart screen functional

--- app/src/main/java/com/example/ecommerceapp/CartScreen.kt
+++ app/src/main/java/com/example/ecommerceapp/CartScreen.kt
@@ -6,13 +6,16 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -31,7 +34,8 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 
 class CartScreen {
-
+      val counter=0;
+      val totalPrice=0;
 
     @Composable
     fun displayCartItems(){
@@ -43,46 +47,53 @@ class CartScreen {
         Row(
             modifier = Modifier.fillMaxWidth()
                 .wrapContentHeight()
-                .padding(10.dp)
+                .padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
         ){
-            Text("My Cart", style = TextStyle(fontSize= 20.sp))
+            Text("My Cart", style = TextStyle(fontSize= 25.sp))
+            Text("Total Price : ", style = TextStyle(fontSize = 20.sp), modifier = Modifier.padding(5.dp))
+
 
         }
 
-        fetchCartItems(viewModel)
+         Spacer(modifier = Modifier.height(20.dp))
 
-    }
+            val cartItemsState = viewModel.cartItems?.observeAsState(emptyList())
+            val totalItems= cartItemsState?.value ?: emptyList()
 
+            if(totalItems.isEmpty()){
+                Column (modifier = Modifier.padding(10.dp).fillMaxHeight().fillMaxWidth().background(color = Color.White), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
+                    Image(painter = painterResource(R.drawable.emptycart), contentDescription = "cart empty", modifier = Modifier.width(200.dp).height(200.dp))
+                }
+            }else{
 
-    @Composable
-    fun fetchCartItems(viewModel: MyViewModel){
-        val cartItemsState = viewModel.cartItems?.observeAsState(emptyList())
-        val totalItems = cartItemsState?.value ?: emptyList()
+                LazyColumn (modifier = Modifier.padding(10.dp).fillMaxWidth().fillMaxHeight(), verticalArrangement = Arrangement.spacedBy(10.dp)){
+                    items(totalItems){
+                            item->
+                        DisplayCard(item)
 
-        if(totalItems.isEmpty()){
-            Column (modifier = Modifier.padding(10.dp).fillMaxHeight().fillMaxWidth().background(color = Color.White), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
-                Image(painter = painterResource(R.drawable.emptycart), contentDescription = "cart empty", modifier = Modifier.width(200.dp).height(200.dp))
-            }
-        }else{
-            LazyColumn (modifier = Modifier.padding(10.dp).fillMaxWidth().fillMaxHeight(), verticalArrangement = Arrangement.spacedBy(10.dp)){
-                items(totalItems){
-                    item->
-                    DisplayCard(item,viewModel)
-
+                    }
                 }
             }
+
+
+
+
         }
 
 
-    }
 
     @Composable
     fun DisplayCard(item: EcommTable){
+
 
         Column (
             modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight()
+                .offset(0.dp,50.dp)
+                .padding(5.dp)
+
 
         ){
             Card(modifier = Modifier
@@ -91,7 +102,7 @@ class CartScreen {
                 .border(2.dp,Color.LightGray),
                 colors = CardDefaults.cardColors(
                 containerColor = Color.White
-            ), shape = RoundedCornerShape(30.dp),
+            ), shape = RoundedCornerShape(5.dp),
                 elevation = CardDefaults.cardElevation(10.dp)
             ){
                 Row(modifier = Modifier.fillMaxWidth()){
